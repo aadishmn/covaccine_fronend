8bc3f33706f5f2b9ec24d22f5b06d41e1dc4df8e
aadishmn <83083983+aadishmn@users.noreply.github.com>
2023-05-08 16:51:17 +0530

    Update AddCenter.js

--- src/pages/AddCenter.js
+++ src/pages/AddCenter.js
@@ -8,7 +8,7 @@ const AddCenter = () => {
   const handleFinish = async (values) => {
     try {
       console.log(values.timing[0].format("HH:MM"));
-      const res = await axios.post("https://covaccine.onrender.com/api/v1/admin/addCenter", {
+      const res = await axios.post("https://long-jade-beanie.cyclic.app/api/v1/admin/addCenter", {
         ...values,
 
         timing: [
